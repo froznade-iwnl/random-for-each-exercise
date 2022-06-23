06b4e801478630e683a04a17c5a0a2d13a6a22ef
Raditya <raditya.learner@gmail.com>
2022-06-23 11:03:45 +0800

    Initial Commit

---
+++ RandomLoopThings/RandomLoopThingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomLoopThingsApp.swift
+//  RandomLoopThings
+//
+//  Created by CTSS Students on 23/6/22.
+//
+
+import SwiftUI
+
+@main
+struct RandomLoopThingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RandomLoopThingsUITests/RandomLoopThingsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  RandomLoopThingsUITestsLaunchTests.swift
+//  RandomLoopThingsUITests
+//
+//  Created by CTSS Students on 23/6/22.
+//
+
+import XCTest
+
+class RandomLoopThingsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
