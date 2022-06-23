7b9d5e317e81b664ccb0777cd8160e26fe487d24
Raditya <raditya.learner@gmail.com>
2022-06-23 11:20:36 +0800

    first

--- RandomLoopThings/ContentView.swift
+++ RandomLoopThings/ContentView.swift
@@ -9,8 +9,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        ZStack{
+            
+            HStack {
+                VStack {
+                    ForEach(1..<10){ _ in
+                        Text("🤣")
+                            .opacity(0.4)
+                            .font(.system(size: 126))
+                        
+                    }
+                }
+                VStack {
+                    ForEach(1..<10){ _ in
+                        Text("😆")
+                            .opacity(0.4)
+                            .font(.system(size: 126))
+                            .offset(y: 75)
+                        
+                    }
+                }
+                VStack {
+                    ForEach(1..<10){ _ in
+                        Text("🤯")
+                            .opacity(0.4)
+                            .font(.system(size: 126))
+
+                        
+                    }
+                }
+                
+            }.ignoresSafeArea(.all)
+            
+            
+        }
+        
     }
 }
 
